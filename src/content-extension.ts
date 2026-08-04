65e2d384e19c983bd365121e6e0acc08b5fecb02
johorta <31520098+johorta@users.noreply.github.com>
2026-08-04 13:04:10 -0400

    Register remaining Agentic Architecture content

--- src/content-extension.ts
+++ src/content-extension.ts
@@ -1,7 +1,9 @@
 import { questions } from './data'
 import { lessons } from './lessons'
 import { agenticLessonsPart2 } from './lessons-agentic-part2'
+import { agenticLessonsPart3 } from './lessons-agentic-part3'
 import { agenticQuestionsPart2 } from './questions-agentic-part2'
+import { agenticQuestionsPart3 } from './questions-agentic-part3'
 
-Object.assign(lessons, agenticLessonsPart2)
-questions.push(...agenticQuestionsPart2)
+Object.assign(lessons, agenticLessonsPart2, agenticLessonsPart3)
+questions.push(...agenticQuestionsPart2, ...agenticQuestionsPart3)
