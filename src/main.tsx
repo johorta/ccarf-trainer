b0bc95e37523fe4f4173a9c90004ff599f32b590
johorta <31520098+johorta@users.noreply.github.com>
2026-08-04 13:29:46 -0400

    Publish English version at separate hash URL

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,27 @@ import ReactDOM from 'react-dom/client'
 import { registerSW } from 'virtual:pwa-register'
 import './content-extension'
 import App from './App'
+import AppEnglish from './AppEnglish'
 import './styles.css'
 import './course.css'
 import './exams.css'
 
 registerSW({ immediate: true })
 
+function Root() {
+  const [hash, setHash] = React.useState(window.location.hash)
+
+  React.useEffect(() => {
+    const updateRoute = () => setHash(window.location.hash)
+    window.addEventListener('hashchange', updateRoute)
+    return () => window.removeEventListener('hashchange', updateRoute)
+  }, [])
+
+  return hash.startsWith('#/en') ? <AppEnglish /> : <App />
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
 )
