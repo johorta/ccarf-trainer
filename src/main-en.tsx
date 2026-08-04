27fed7814fd22bb96d6291486c23dcf8c9453fa9
johorta <31520098+johorta@users.noreply.github.com>
2026-08-04 16:00:17 -0400

    Enable QR deep-link import in English app

--- src/main-en.tsx
+++ src/main-en.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { registerSW } from 'virtual:pwa-register'
 import './content-extension'
 import AppEnglish from './AppEnglish'
+import { StoredProgressTransfer } from './progress-transfer'
 import './styles.css'
 import './course.css'
 import './exams.css'
@@ -19,7 +20,12 @@ function EnglishRoot() {
     return () => window.removeEventListener('hashchange', handleHashChange)
   }, [])
 
-  return <AppEnglish />
+  return (
+    <>
+      <AppEnglish />
+      <StoredProgressTransfer locale="en" storageKey="ccarf-trainer-progress-en-v1" />
+    </>
+  )
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
