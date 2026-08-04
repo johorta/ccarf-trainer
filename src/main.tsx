0905a535d7d4915dcfb3da02ec9edc4f608338c9
johorta <31520098+johorta@users.noreply.github.com>
2026-08-04 12:53:12 -0400

    Load expanded course content

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { registerSW } from 'virtual:pwa-register'
+import './content-extension'
 import App from './App'
 import './styles.css'
 import './course.css'
